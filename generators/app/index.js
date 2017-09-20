f6665673382881a6c39eef8953e6d30c4ae3ab1d
Wojciech Trocki <wtrocki@redhat.com>
2017-09-20 22:32:41 +0100

    Wip generator code

---
+++ generators/app/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require('path');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay('' + chalk.red('RainCatcher Step') + ' generator!'
+    ));
+
+    const array = this.env.cwd.split('/');
+    const prompts = [{
+      type: 'input',
+      name: 'appName',
+      message: 'Your step name',
+      // Defaults to the project's folder name if the input is skipped
+      default: this.appName || array[array.length - 1]
+    }, {
+      type: 'confirm',
+      name: 'mkdirBool',
+      message: 'Would you like to create a folder?',
+      default: true
+    }];
+
+    return this.prompt(prompts.slice(0, 2)).then(props => {
+      const done = this.async();
+      this.inputs = props;
+      this.appName = this.inputs.appName.trim().split(' ').join('-');
+
+      return this.prompt(prompts[2]).then(prop => {
+        this.inputs.mkdirBool = prop.mkdirBool;
+        this.mkdirBool = prop.mkdirBool;
+        done();
+      })
+      .catch(err => console.error(err));
+    });
+  }
+
+  writing() {
+    // Change cwd based on prompt
+    let copyPath = this.env.cwd;
+    if (this.mkdirBool) {
+      copyPath = this.appName;
+    }
+
+    // Copy package.json
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath(path.join(copyPath, 'package.json')), {
+        appName: this.appName
+      }
+    );
+
+    // All other files & directories
+    this.fs.copy(
+      [
+        this.templatePath('**.!(json)'),
+        this.templatePath('app'),
+        this.templatePath('db'),
+        this.templatePath('public'),
+        this.templatePath('server')
+      ], this.destinationPath(copyPath)
+    );
+  }
+
+  install() {
+    // Install dependencies in new directory
+    const npmdir = path.join(process.cwd(), this.appName);
+    if (this.mkdirBool) {
+      process.chdir(npmdir);
+    }
+    this.npmInstall();
+  }
+};
